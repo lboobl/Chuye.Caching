a33f5bc676ce69c484d252526dd8cab05881afca
Jusfr <jusfr.w@gmail.com>
2015-04-09 21:32:39 +0800

    添加 Chuye.Caching 的 MSBuild bat

---
+++ build_Chuye_Caching.cmd
@@ -0,0 +1,18 @@
+@setlocal 
+@set local=%~dp0
+@pushd "C:\Windows\Microsoft.NET\Framework\v4.0.30319"
+@goto build
+
+
+:build
+msbuild %local%src\Chuye.Caching.sln /t:Rebuild /P:Configuration=Release
+@goto copy
+
+:copy
+robocopy %local%src\Chuye.Caching\bin\Release %local%release /e
+robocopy %local%src\Chuye.Caching.Memcached\bin\Release %local%release /e
+@goto end
+
+:end
+@pushd %local%
+@pause
