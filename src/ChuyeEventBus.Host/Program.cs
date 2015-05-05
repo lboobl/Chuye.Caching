ab171b69148a86c2df837061213bc73e48f1abe7
Jusfr <jusfr.v@gmail.com>
2015-05-05 19:10:44 +0800

    将事件Mock逻辑移入Demo项目

--- src/ChuyeEventBus.Host/Program.cs
+++ src/ChuyeEventBus.Host/Program.cs
@@ -1,5 +1,4 @@
 ﻿using ChuyeEventBus.Core;
-using ChuyeEventBus.Demo;
 using NLog;
 using System;
 using System.Diagnostics;
@@ -25,8 +24,8 @@ namespace ChuyeEventBus.Host {
             var server = StartServer();
             _logger.Trace("Press <ctrl + c> to abort, <Enter> to stop");
 
-            //MockClient();
-            //MockClientAsync();
+            //ChuyeEventBus.Demo.EventMocker.MockClient();
+            //ChuyeEventBus.Demo.EventMocker.MockClientAsync();
 
             Console.ReadLine();
             server.Stop();
@@ -43,34 +42,6 @@ namespace ChuyeEventBus.Host {
             server.EventHandlerFinder = finder;
             server.StartAsync();
             return server;
-        }
-
-        static void MockClient() {
-            for (int i = 0; i < 6; i++) {
-                MessageQueueUtil.Send(new FansFollowEvent() { FromId = 1, ToId = i + 1 });
-            }
-            for (int i = 0; i < 8; i++) {
-                MessageQueueUtil.Send(new WorkPublishEvent() { WorkId = 60 + i * 10 });
-            }
-        }
-
-        static void MockClientAsync() {
-            var random = new Random();
-            var works = Enumerable.Range(0, 100).Select(r => random.Next(1, 100)).ToArray();
-            Task.Run(action: () => {
-                for (int i = 0; i < 100; i++) {
-                    MessageQueueUtil.Send(new WorkPublishEvent() { WorkId = i });
-                    Thread.Sleep(Math.Abs(Guid.NewGuid().GetHashCode()) % 200 + 300);
-                }
-            });
-
-            //Task.Run(action: () => {
-            //    while (true) {
-            //        var id = works[Math.Abs(Guid.NewGuid().GetHashCode()) % works.Length];
-            //        MessageQueueUtil.Send(new FansFollowEvent() { FromId = 1, ToId = 2 });
-            //        Thread.Sleep(Math.Abs(Guid.NewGuid().GetHashCode() % 1000 + 1000));
-            //    }
-            //});
         }
     }
 }
