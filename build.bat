69a957d553cef6a7b82b9346f80cf6a9a7f877b7
jusfr <jusfr.v@gmail.com>
2016-04-01 12:30:22 +0800

    Remove extra src and packages, add nuget package scripts

---
+++ build.bat
@@ -0,0 +1,24 @@
+@setlocal 
+@set local=%~dp0
+
+@pushd %WINDIR%\Microsoft.NET\Framework\v4.0.30319\
+@goto build
+
+:build
+msbuild "%local%src\Chuye.Caching.sln" /t:Rebuild /P:Configuration=Release
+@goto copy
+
+:copy
+robocopy "%local%src\Chuye.Caching.Memcached\bin\Release" "%local%release\Chuye.Caching.Memcached" /mir
+robocopy "%local%src\Chuye.Caching.Redis\bin\Release" "%local%release\Chuye.Caching.Redis" /mir
+@pack end
+
+:pack
+@pushd "%local%"
+.nuget\NuGet.exe pack build\Chuye.Caching.Memcached.nuspec -Prop Configuration=Release -OutputDirectory release
+.nuget\NuGet.exe pack build\Chuye.Caching.Redis.nuspec -Prop Configuration=Release -OutputDirectory release
+@goto end
+
+:end
+@pushd %local%
+@pause
